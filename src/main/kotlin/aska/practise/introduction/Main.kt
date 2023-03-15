6433b9cbd7268f58ff904f945ac894f008c7b305
azicha <askakauchi@gmail.com>
2023-03-15 11:47:41 +0300

    First commit

---
+++ src/main/kotlin/aska/practise/introduction/Main.kt
@@ -0,0 +1,15 @@
+package aska.practise.introduction
+
+fun main() {
+    println("Hello World!");
+    val y: Int = mySum(34, 10);
+
+    println("Sum is $y");
+}
+
+fun mySum(x: Int, y: Int): Int{
+    return x + y
+}
+
+
+
