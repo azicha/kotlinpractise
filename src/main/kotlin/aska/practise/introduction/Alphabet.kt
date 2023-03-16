e5672d714904e88185f967fe1c04ee2a073dadc3
azicha <askakauchi@gmail.com>
2023-03-16 19:05:44 +0300

    updating code to print alphabet

---
+++ src/main/kotlin/aska/practise/introduction/Alphabet.kt
@@ -0,0 +1,36 @@
+ package aska.practise.introduction
+
+ fun main() {
+     a()
+
+ }
+
+ fun a() {
+     val alpha: String = "a"
+     println("My alphabet is $alpha")
+     b()
+ }
+
+ fun b() {
+     val alpha: String = "b"
+     println("My alphabet is $alpha")
+     c()
+ }
+
+ fun c() {
+     val alpha: String = "c"
+     println("My alphabet is $alpha")
+     d()
+ }
+
+ fun d() {
+     val alpha: String = "d"
+     println("My alphabet is $alpha")
+     e()
+ }
+
+ fun e() {
+     val alpha: String = "e"
+     println("My alphabet is $alpha")
+
+ }
