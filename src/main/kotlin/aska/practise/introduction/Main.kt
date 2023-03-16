fafb10f244113e441fb62771bd9ba8b903aa6243
azicha <askakauchi@gmail.com>
2023-03-16 18:44:14 +0300

    First Kotlin Program

--- src/main/kotlin/aska/practise/introduction/Main.kt
+++ src/main/kotlin/aska/practise/introduction/Main.kt
@@ -1,14 +1,35 @@
 package aska.practise.introduction
 
 fun main() {
-    println("Hello World!");
-    val y: Int = mySum(34, 10);
-
-    println("Sum is $y");
+   println(sum(12, 6))
 }
 
-fun mySum(x: Int, y: Int): Int{
-    return x + y
+fun sum(x: Int, y: Int) : Int {
+    val result: Int = x + y
+    multiply(23, result)
+
+    return result
+}
+
+fun multiply(x: Int, y: Int) : Int {
+    val result: Int = x * y
+    println("Multiplication is $result")
+    divide(900, result)
+
+    return result
+
+}
+
+fun divide(x: Int, y: Int) : Int {
+    val result: Int = x / y
+    println("Division is $result")
+    subtraction(40, result)
+
+    return result
+}
+
+fun subtraction(x: Int, y: Int) {
+    println("Subtraction is ${x - y}")
 }
 
 
